75e645186eb13aa063cc9b1e71e40a194470def5
sumit-tt <sumit.patil@tradingtechnologies.com>
2018-01-05 15:17:39 -0600

    save changes

--- com.tt.js/test/ClientTest.py
+++ com.tt.js/test/ClientTest.py
@@ -2,25 +2,26 @@ __author__ = 'spatil'
 
 #code for client test application goes here.
 
-# Python program to check if the input number is prime or not
+# Python asdasd program to check if the input number is prime or not asdsdasd
 
-num = 407
+num = 240
 
 # take input from the user
 # num = int(input("Enter a number: "))
 
 # prime numbers are greater than 1
 if num > 1:
-   # check for factors
+
+   # asdd check for factors
    for i in range(2,num):
        if (num % i) == 0:
-           print(num,"is not a prime number")
+           print(num,"is not  prime number")
            print(i,"times",num//i,"is",num)
            break
    else:
-       print(num,"is a prime number")
+       print(num,"is  prime number")
 
-# if input number is less than
-# or equal to 1, it is not prime
-else:
-   print(num,"is not a prime number")
+# adsasdsad if input number is less than or equal to 1, it is not prime
+
+else: #sadada
+   print(num,"is   not  prime number")
--- com.tt.ui/test/TestloadWS.py
+++ com.tt.ui/test/TestloadWS.py
@@ -3,10 +3,10 @@ __author__ = 'spatil'
 #Code for load ws test goes here.
 
 
-# Python program to find the factorial of a number provided by the user.
+# Python program to find the factorial of a number provided by the user.  asdsadasdf
 
-# change the value for a different result
-num = 7
+# change the value for a different result asdasd a
+num = 4
 
 # uncomment to take input from the user
 #num = int(input("Enter a number: "))
@@ -15,10 +15,10 @@ factorial = 1
 
 # check if the number is negative, positive or zero
 if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
+   print("Sorry,asdasda factorial does not exist for negative numbers")
 elif num == 0:
-   print("The factorial of 0 is 1")
+   print("Theadasd factorial of 0 is 1")
 else:
    for i in range(1,num + 1):
        factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
+   print("The facasdasdtorial of",num,"is",factorial)
